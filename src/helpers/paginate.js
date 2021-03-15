e824641fc519d6ea5b08fe3c47e583426002a87a
William Guevara <will.dev@hotmail.com>
2021-03-15 16:42:03 -0500

    refactor: added check to paginate function to disallow a page number with no data

--- src/helpers/paginate.js
+++ src/helpers/paginate.js
@@ -6,13 +6,20 @@
  * @returns {Array<object>} Data to display on the page
  */
 const paginateHeroes = (data, sizePage, numberPage) => {
-	const min = (numberPage - 1) * sizePage;
-	const max = sizePage * numberPage;
-	const page = data.filter((hero, i) => {
+	const length = data.length;
+	const page = verifyNumberPage(length, sizePage, numberPage);
+	const min = (page - 1) * sizePage;
+	const max = sizePage * page;
+	const pageData = data.filter((hero, i) => {
 		return i >= min && i < max;
 	});
 
-	return page;
+	return pageData;
+};
+
+const verifyNumberPage = (length, sizePage, numberPage) => {
+	const maxNumberPage = Math.ceil(length / sizePage);
+	return numberPage > maxNumberPage ? maxNumberPage : numberPage;
 };
 
 export default paginateHeroes;
